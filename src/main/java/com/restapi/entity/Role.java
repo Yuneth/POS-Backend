b7ade60af46d93430952c453afca40fc02b765c7
Yuneth <yunethperera987@gmail.com>
2024-05-19 16:55:03 +0530

    1st commit

---
+++ src/main/java/com/restapi/entity/Role.java
@@ -0,0 +1,32 @@
+package com.restapi.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+	@Id
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
